a3fa15e9cd847b779fefd1a1dccc0b9a7667e8d9
Dmitry Chmerenko <dchmerenko@gmail.com>
2020-06-22 16:56:33 +0300

    Parser.py initial commit

---
+++ smart_calculator/Parser.py
@@ -0,0 +1,54 @@
+# parser.py
+
+
+class Parser:
+    '''
+    Parser converts infix notation to postfix notation (Reverse Polish Notation)
+    >>> p = Parser('5-3+2')
+    >>> p.expr()
+    53-2+
+    '''
+    def gen(self):
+        for w in self.infix_str:
+            yield w
+
+    def __init__(self, string):
+        self.infix_str = string
+        self.g = self.gen()
+        self.lookahead = self.read()
+
+    def read(self):
+        return next(self.g, None)
+
+    def expr(self):
+        self.term()
+        while True:
+            if self.lookahead == '+':
+                self.match('+')
+                self.term()
+                print('+', end='')
+            elif self.lookahead == '-':
+                    self.match('-')
+                    self.term()
+                    print('-', end='')
+            else:
+                return
+
+    def term(self):
+        if self.lookahead.isdigit():
+            print(self.lookahead, end='')
+            self.match(self.lookahead)
+        else:
+            print('Sintax error', self.lookahead)
+
+    def match(self, t):
+        if self.lookahead == t:
+            self.lookahead = self.read()
+        else:
+            print('Sintax error', self.lookahead)
+
+
+if __name__ == '__main__':
+    p = Parser('5-3+2')
+    p.expr()
+    print()
